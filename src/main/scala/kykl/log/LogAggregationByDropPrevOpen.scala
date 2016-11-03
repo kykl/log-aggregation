82261d81c90bf12efb089faaeb25ef21ab722eb1
Kenneth Lee <kenneth.lee@gmail.com>
2016-11-03 11:04:24 -0700

    created and used reduce function

--- src/main/scala/kykl/log/LogAggregationByDropPrevOpen.scala
+++ src/main/scala/kykl/log/LogAggregationByDropPrevOpen.scala
@@ -8,44 +8,46 @@ case class UserTimeSpent(uid:Int, timeSpent:Double) {
 
 /** Log aggreation interface to compute average */
 trait LogAggregation {
+  case class UserTotalTimeSpent(uid: Int, totalTimeSpent: Long = 0, numberOfSessions: Int = 0, lastOpen: Option[Long] = None) {
+    def average = if (numberOfSessions <= 0) 0.0 else totalTimeSpent.toDouble / numberOfSessions
+    override def toString = s"{${uid}, ${average}}"
+  }
+
+  def reduceFn(result:UserTotalTimeSpent, ua:UserLogEntry):UserTotalTimeSpent = {
+    result.lastOpen match {
+      case None =>
+        // first open
+        if (ua.isOpen)
+          UserTotalTimeSpent(result.uid, result.totalTimeSpent, result.numberOfSessions, Some(ua.timestamp))
+        else
+          result
+      case Some(lastOpen) =>
+        if (ua.isOpen)
+        // drop previous open
+          UserTotalTimeSpent(result.uid, result.totalTimeSpent, result.numberOfSessions, Some(ua.timestamp))
+        else
+          UserTotalTimeSpent(result.uid, result.totalTimeSpent + (ua.timestamp - lastOpen), result.numberOfSessions + 1, None)
+    }
+  }
 
   /** Returns time spent for each users in log entries
     *
     * @param entries user log entires
     */
-  def average(entries:Seq[UserLogEntry]):Seq[UserTimeSpent]
+  def average(entries:Seq[UserLogEntry])(implicit reduce: (UserTotalTimeSpent, UserLogEntry) => UserTotalTimeSpent = reduceFn):Seq[UserTimeSpent]
 }
 
 /** This log aggregation drops previous open entries when it cannot find matching close entries */
 class LogAggregationByDropPrevOpen extends LogAggregation {
-  private case class UserTotalTimeSpent(uid: Int, totalTimeSpent: Long = 0, numberOfSessions: Int = 0, lastOpen: Option[Long] = None) {
-    def average = if (numberOfSessions <= 0) 0.0 else totalTimeSpent.toDouble / numberOfSessions
-    override def toString = s"{${uid}, ${average}}"
-  }
 
-  override def average(entries: Seq[UserLogEntry]): Seq[UserTimeSpent] = {
+
+  override def average(entries: Seq[UserLogEntry])(implicit reduce: (UserTotalTimeSpent, UserLogEntry) => UserTotalTimeSpent): Seq[UserTimeSpent] = {
     // group by user id
     entries.groupBy(_.uid).map{ kv =>
       val (uid, actions) = kv
 
       // reduce by sum up total session time (close timestamp - open timestamp)
-      actions.foldLeft(UserTotalTimeSpent(uid)) { (result, ua) =>
-        val current = result.lastOpen match {
-          case None =>
-            // first open
-            if (ua.isOpen)
-              UserTotalTimeSpent(result.uid, result.totalTimeSpent, result.numberOfSessions, Some(ua.timestamp))
-            else
-              result
-          case Some(lastOpen) =>
-            if (ua.isOpen)
-              // drop previous open
-              UserTotalTimeSpent(result.uid, result.totalTimeSpent, result.numberOfSessions, Some(ua.timestamp))
-            else
-              UserTotalTimeSpent(result.uid, result.totalTimeSpent + (ua.timestamp - lastOpen), result.numberOfSessions + 1, None)
-        }
-        current
-      }
+      actions.foldLeft(UserTotalTimeSpent(uid))((ts, ua) => reduce(ts, ua))
     }.map { session => UserTimeSpent(session.uid, session.average) }.toSeq
   }
 }
